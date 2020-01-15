9d06db92b8c3174d847b70f960740785ab958c00
Ubuntu <ubuntu@ip-172-31-23-168.ec2.internal>
2020-01-15 15:24:00 +0000

    added font cdn and removed it from static

--- bc_shop/settings.py
+++ bc_shop/settings.py
@@ -171,7 +171,8 @@ MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN,  MEDIAFILES_LOCATION)
+
 
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
