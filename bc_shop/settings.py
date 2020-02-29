8158199d7d8abc7b81ef8a1b96aae015dc29294c
Ubuntu <ubuntu@ip-172-31-23-168.ec2.internal>
2020-02-29 19:41:21 +0000

    changed the debug mode

--- bc_shop/settings.py
+++ bc_shop/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['2b61fd793c044769afcecb41f18d2796.vfs.cloud9.us-east-1.amazonaws.com', 'tanias-shop.herokuapp.com']
     
